c4ff9abc3bcf393383d6788bb0e2d76eddbe89f0
Genesekt66 <easchleter@gmail.com>
2025-09-17 09:56:39 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Predictive_Lifestyle_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
