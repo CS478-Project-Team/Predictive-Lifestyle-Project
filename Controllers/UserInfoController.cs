8973b9e301e643a84e9fc0d1e2ff751a364d1acc
Bryce <brycehazelip@gmail.com>
2025-10-15 09:28:13 -0500

    Added UserInput web page with a few text boxes.

---
+++ Controllers/UserInfoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Predictive_Lifestyle_Project.Models;
+
+namespace Predictive_Lifestyle_Project.Controllers
+{
+    public class UserInfoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(UserInfo model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Message = $"Age: {model.Age}, Weight: {model.Weight}, Height: {model.Height}";
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ Models/UserInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace Predictive_Lifestyle_Project.Models
+{
+    public class UserInfo
+    {
+        public int Age { get; set; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+    }
+}
