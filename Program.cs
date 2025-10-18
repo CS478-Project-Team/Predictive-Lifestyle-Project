853c5cef7ea585de145ea23a5a6e9b9039e6487f
GavinDavis-coder <64231400+GavinDavis-coder@users.noreply.github.com>
2025-10-18 13:21:33 -0500

    Created SQL Server to handle user data, User inputs, and training data. Changed connection strings in apsettings.json and Program.cs to point to the proper database.

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
     ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
