e986f79f902aa775cf1f9af8c4a9e5be20a7fd06
GavinDavis-coder <gdavis1804@gmail.com>
2025-11-04 22:19:38 -0600

    Changed max caloric intake to 10,000 instead of 250. Allowed for a minimum of 0 alcoholic drinks insead of greater than or equal to 1.

--- Models/ViewModels/HealthEntryViewModel.cs
+++ Models/ViewModels/HealthEntryViewModel.cs
@@ -8,9 +8,9 @@ namespace Predictive_Lifestyle_Project.Models.ViewModels
         [Required, RegularExpression("M|F|O")] public string SexAtBirth { get; set; } = "O";
         [Range(100, 250)] public decimal HeightIn { get; set; }
         [Range(30, 300)] public decimal WeightLbs { get; set; }
-        [Range(60, 250)] public int? DailyCal { get; set; }
+        [Range(60, 10000)] public int? DailyCal { get; set; }
         [Range(30, 200)] public int? RestingHr { get; set; }
-        [Range(1, 200)] public int? AlcoholicDrinksPerWeek { get; set; }
+        [Range(0, 200)] public int? AlcoholicDrinksPerWeek { get; set; }
         public int? StepsPerDay { get; set; }
         public decimal? SleepHours { get; set; }
         [Required, RegularExpression("Y|N")] public string SmokeOrVape { get; set; } = "N";
