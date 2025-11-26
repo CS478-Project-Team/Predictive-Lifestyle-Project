d065b573151d08a54ab393e55cbda2c5f4692dae
GavinDavis-coder <gdavis1804@gmail.com>
2025-11-25 20:42:26 -0600

    fixed some errors with prediction storage values.

---
+++ Data/Migrations/20251126022934_FixPredictionScoreType.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Predictive_Lifestyle_Project.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixPredictionScoreType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Score",
+                table: "Predictions",
+                type: "decimal(18,4)",   // or decimal(5,4) if you only need 0..1
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
